55376858f75a1cb8683c5f4b096be312ace5d44f
Thinglink <Thinglink@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-22 11:19:16 +0000

    tagging 1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/thinglink/tags/1.0.3@363214 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ rjw-thinglink-config.php
@@ -0,0 +1,45 @@
+<div class="wrap">
+	<?php if ($_GET['settings-updated']) { ?>
+		<div id="message" class="updated fade" style="margin: 15px 0 10px;"><p>Your settings have been <strong>saved</strong>.</p></div>
+	<?php } ?>
+
+		   <h2><?php echo($plugin_name) ?></h2>
+
+   <form method="post" action="options.php">
+   <?php settings_fields( 'rjw-thinglink-group' ); ?>
+   <?php do_settings_sections( 'rjw-thinglink-group' ); ?>
+   
+
+    <div id="login_notification" style="margin: 20px; border: 2px solid black;width: 320px;padding:10px; text-align:center;">
+    	You can find out your ID by logging on to <a href="http://www.thinglink.com">Thinglink</a>.
+    </div>
+   
+    <table class="form-table">
+        <tr valign="top">
+        <th scope="row"><b>Your Thinglink ID:</b></th>
+        <td><input type="text" id="thinglink_id_input" name="thinglink_id" value="<?php echo(get_option('thinglink_id')); ?>" style="width:150px;"/></td>
+        </tr>
+    </table>
+    
+    <p class="submit">
+    <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+    </p>
+
+   </form>
+   
+   <script type="text/javascript">
+   function prefill(obj) {
+	   if(obj["name"] && jQuery('#thinglink_id_input').val() == "") {
+		   jQuery('#thinglink_id_input').val(obj["embedCode"]);
+	   }
+
+	   if(obj["name"]) {
+		   jQuery("#login_notification").html("You're currently logged on to <a href='http://www.thinglink.com'>Thinglink</a> as <b>" + obj["name"]+ "</b> with Thinglink ID <b>" + obj["embedCode"]+ "</b>");
+	   }
+   }
+   </script>
+   
+   <script type="text/javascript" src="http://www.thinglink.com/api/me?callback=prefill" > </script>
+
+   
+</div>
